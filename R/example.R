7dc83483335713b5054a1ac966455d8449c696b7
Chris Hodapp <hodapp87@gmail.com>
2017-01-02 11:05:36 -0500

    Added some information to README.md, and basic R code

---
+++ R/example.R
@@ -0,0 +1,25 @@
+#!/usr/bin/env Rscript
+
+## install.packages("rpart")
+
+library(rpart)
+
+## Read in CSV data, and drop "id" column since it's not needed:
+train <- read.table("winequality-data.csv", sep=",", header=TRUE)
+train$id <- NULL
+
+## Fit a decision tree, modeling the 'quality' column from every other
+## attribute:
+tree <- rpart(quality ~ ., data = train)
+
+## Read in "solution" input:
+soln <- read.table("winequality-solution-input.csv", sep=",",
+                   header=TRUE)
+
+soln_predict <- predict(tree, soln)
+
+## Format the predictions as a submission:
+submission <- data.frame(id = soln$id,
+                         quality = soln_predict)
+write.table(submission, sep=",", quote=FALSE, row.names = FALSE,
+            "winequality-submission.csv")
--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # cincyfp_kaggle_201701
+
+This contains some example implementations for the Kaggle competition
+done at the CincyFP meeting on January 10th, 2016.
+See <https://inclass.kaggle.com/c/uci-wine-quality-dataset> to
+download the data files.
+
+## R
+
+See `example.R` in the `R` directory.  This uses
+the [rpart](https://cran.r-project.org/web/packages/rpart/index.html)
+library to fit a decision tree to the data.
+
+For those not familiar with R, you'll probably need to first run:
+
+```r
+install.packages("rpart")
+```
