39d0939119c3bfea3a21b0eac16890110674f42f
gigasquid <cmeier@gigasquidsoftware.com>
2017-01-09 18:47:37 -0500

    starter wine quality solution with cortex

--- Clojure/wine-quality/project.clj
+++ Clojure/wine-quality/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.csv "0.1.3"]
-                 [nz.ac.waikato.cms.weka/weka-stable "3.6.12"]])
+                 [nz.ac.waikato.cms.weka/weka-stable "3.6.12"]
+                 [thinktopic/cortex.suite "0.3.1-SNAPSHOT"]])
---
+++ Clojure/wine-quality/src/wine_quality/cortex.clj
@@ -0,0 +1,134 @@
+(ns wine-quality.cortex
+  (:require [clojure.data.csv :as csv]
+            [clojure.java.io :as io]
+            [cortex.dataset :as ds]
+            [cortex.loss :as loss]
+            [cortex.nn.execute :as execute]
+            [cortex.nn.layers :as layers]
+            [cortex.nn.network :as network]
+            [cortex.nn.traverse :as traverse]
+            [cortex.optimise :as opt]
+            [think.compute.nn.compute-execute :as ce]
+            [think.compute.nn.cpu-backend :as cpu-backend]
+            [think.resource.core :as resource]
+            [wine-quality.core :as wine-quality]))
+
+;;; An attempt at using Cortex for the wine quality data
+;;; https://github.com/thinktopic/cortex
+
+;; Notes from Googe Group
+
+;; Just a couple of quick notes. This is the type of problem that is typically a better fit for a decision tree or regression approach, or an ensemble thereof like adaboost/gradient boosting or random forests. That said, a neural network with no hidden layers and a softmax output is essentially equivalent to logistic regression, or basic regression with a linear output, and could be a good starter example.
+
+;; There would still be a few gotchas in approaching this dataset, but they could just as easily be teaching opportunities rather than problems. I'm thinking specifically that you'd probably want to take steps to balance the dataset (roughly equal number of samples per class per training batch) and normalize the input, e.g. to values from 0.0 to 1.0 or -1.0 to 1.0. It looks like mostly the input values are quantities rather than categorical, so you likely won't have to worry about encoding categorical variables. If I missed something in scanning though and there are categorical values, you'd probably want to encode those as dummies.
+
+;; From there you could experiment with doing things like adding a hidden layer to see if feature combinations are helpful. You could also use an l2 penalty/constraint on the weights as a form of regularization. With a hold out validation and/or test dataset this is likely to make training and validation error more similar than they'd be w/o the l2 penalty.
+
+
+(defn- train-and-get-results
+  [context network input-bindings output-bindings
+   batch-size dataset optimiser disable-infer? infer-batch-type
+   n-epochs map-fn]
+  (let [output-id (ffirst output-bindings)]
+    (resource/with-resource-context
+      (network/print-layer-summary (-> network
+                                       network/build-network
+                                       traverse/auto-bind-io
+                                       traverse/network->training-traversal))
+      (let [trained-network  (as-> (network/build-network network) net-or-seq
+                               (execute/train context net-or-seq dataset input-bindings output-bindings
+                                              :batch-size batch-size
+                                              :optimiser optimiser
+                                              :disable-infer? disable-infer?
+                                              :infer-batch-type infer-batch-type)
+                               (take n-epochs net-or-seq)
+                               (map map-fn net-or-seq)
+                               (last net-or-seq)
+                               (execute/save-to-network context (get net-or-seq :network) {}))
+            infer-results  (-> (execute/infer-columns context trained-network dataset input-bindings output-bindings
+                                                      :batch-size batch-size)
+                               (get output-id))]
+        {:network trained-network :infer-results infer-results}))))
+
+
+(defn create-context
+  []
+  (ce/create-context
+   #(cpu-backend/create-cpu-backend :float)))
+
+
+;;;;; Wine quality
+
+(def WINE-DATA
+  (->> (rest wine-quality/raw-data)
+       (mapv #(take 11 %))
+       (mapv #(mapv (fn [x] (Double/parseDouble x)) %))))
+
+(def WINE-LABELS
+  (->> (rest wine-quality/raw-data)
+       (mapv #(nth % 11))
+       (mapv #(Double. %))))
+
+(def TEST-WINE-DATA
+  (->> (rest wine-quality/test-data)
+       (mapv #(mapv (fn [x] (Double. x)) %))))
+
+
+(defn train-wine
+  "This trains the network on the wine data and results the Mean squared loss"
+  [context]
+  (let [n-epochs 50
+        dataset (ds/create-in-memory-dataset {:data {:data WINE-DATA
+                                                     :shape 11}
+                                              :labels {:data WINE-LABELS
+                                                       :shape 1}}
+                                             (ds/create-index-sets (count WINE-DATA)
+                                                                   :training-split 1.0
+                                                                   :randomize? false))
+
+        loss-fn (loss/mse-loss)
+        input-bindings [(traverse/->input-binding :input :data)]
+        output-bindings [(traverse/->output-binding :output
+                                                    :stream :labels
+                                                    :loss loss-fn)]
+        results (train-and-get-results context [(layers/input 11 1 1 :id :input)
+                                                (layers/linear 1 :id :output)]
+                                       input-bindings output-bindings 100 dataset
+                                       (opt/adadelta) true nil n-epochs identity)
+        mse (loss/average-loss loss-fn (:infer-results results) WINE-LABELS)]
+    (println "The MSE for the wine training is " mse)
+    (assoc results :mse mse)))
+
+(defn test-wine
+  "Gets the results of a trained network on the test data"
+  [context trained-network batch-size]
+  (let [dataset (ds/create-in-memory-dataset {:data {:data TEST-WINE-DATA
+                                                     :shape 11}
+                                              :labels {:data (into [] (repeat (count TEST-WINE-DATA) 0))
+                                                       :shape 1}}
+                                             (ds/create-index-sets (count TEST-WINE-DATA)))
+        input-bindings [(traverse/->input-binding :input :data)]
+        output-bindings [(traverse/->output-binding :output
+                                                    :stream :labels)]
+        output-id (ffirst output-bindings)
+        infer-results  (-> (execute/infer-columns context trained-network dataset input-bindings output-bindings
+                                                  :batch-size batch-size)
+                           (get output-id))]
+    infer-results))
+
+(defn to-kaggle-results
+  "Writes the test results in the Kaggle format"
+  [results]
+  (with-open [out-file (io/writer "cortex-kaggle-results.csv")]
+    (csv/write-csv out-file
+                    (into [["id" "quality"]]
+                          (mapv (fn [idx r] [(int  idx) (double r)]) (mapv last TEST-WINE-DATA) (mapv first results))))))
+
+(comment
+
+  (def trained-network (train-wine (create-context)))
+
+  (def infer-results (test-wine (create-context) (:network trained-network) 100))
+
+  (to-kaggle-results infer-results)
+)
